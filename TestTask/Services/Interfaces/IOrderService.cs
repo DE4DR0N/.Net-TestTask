867c5a72a4231fb4886a75ef544ae8478012b81d
eugen <eugen@DESKTOP-6RDJ7BN>
2023-10-01 19:11:59 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+
+namespace TestTask.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        public Task<Order> GetOrder();
+
+        public Task<List<Order>> GetOrders();
+    }
+
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            return _context.Orders.OrderByDescending(x => x.Price * x.Quantity).FirstAsync();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return _context.Orders.Where(x => x.Quantity > 10).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Interfaces/IUserService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+
+namespace TestTask.Services.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<User> GetUser();
+
+        public Task<List<User>> GetUsers();
+    }
+
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<User> GetUser()
+        {
+            return _context.Users.OrderByDescending(x => x.Orders.Count).FirstOrDefaultAsync();
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return _context.Users.Where(x => x.Status == Enums.UserStatus.Inactive).ToListAsync();
+        }
+    }
+}
